7d4c147f409fa9e3559a19a530d5aa9da5da0a73
cricketsamya <cricketsamya@gmail.com>
2017-10-20 15:27:33 +0200

    spring mvc boot and jpa web together

--- first-spring-mvc/src/main/java/com/sk/springmvc/LoginController.java
+++ first-spring-mvc/src/main/java/com/sk/springmvc/LoginController.java
@@ -1,14 +1,33 @@
 package com.sk.springmvc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.sk.service.LoginService;
 
 @Controller
 public class LoginController {
 
+	@Autowired
+	private LoginService service;
+
 	@RequestMapping(method = RequestMethod.GET, value = "/login")
-	public String sayHello() {
+	public String showLoginPage() {
+		return "login";
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/login")
+	public String handleLoginRequest(@RequestParam(name = "name") String name,
+			@RequestParam(name = "password") String password, ModelMap model) {
+		if (service.validateUser(name, password)) {
+			model.put("name", name);
+			return "welcome";
+		}
+		model.put("errorMessage", "Invalid Credentials!!");
 		return "login";
 	}
 }
